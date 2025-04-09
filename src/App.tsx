f4073433c98ae46018dc1c85a539e99a7dde2500
sibelephant <masterpiekhalid@gmail.com>
2025-04-09 15:00:34 +0100

    did the left side of the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@
 import {BrowserRouter,Route,Routes } from 'react-router'
 import './index.css'
 import Index from './index.tsx'
+import Dashboard from './pages/Dashboard.tsx'
 
  const App = () => {
    return (
     <BrowserRouter>
     <Routes>
       <Route index path="/" element={<Index />} />
+      <Route index path="/dashboard" element={<Dashboard />} />
+      
     </Routes>
   </BrowserRouter>
    )
